d08895b039a695347d674a34d26d3852c9f6eb34
ResoluteVinculum <trentoberlander@gmail.com>
2026-03-07 17:47:23 -0800

    Pythonic

--- SpellWriting/generation/__init__.py
+++ SpellWriting/generation/__init__.py
@@ -1,12 +1,16 @@
 #
 import inspect
 
+from SpellWriting.generation import edge, node, necklace
+from SpellWriting.abstract import IterEnum
 
 _get_functions = lambda module: [
     (name.capitalize(), func) 
     for name, func in inspect.getmembers(module, inspect.isfunction)]
 
-Nodes = Enum('Nodes', dict(_get_functions(bases)))
 
-
+ordering = {}
+for mod in [node, edge, necklace]:
+    print(mod.__name__)
 # TODO SpellGeneration IterEnum
+SpellGeneration = IterEnum('SpellGeneration', names=tuple())
